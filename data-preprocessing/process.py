48718ed20f9b5e9b5877da7c185392894e6afd0a
113741090a <35476724+113741090a@users.noreply.github.com>
2024-10-22 18:31:01 +0800

    Update process.py

--- data-preprocessing/process.py
+++ data-preprocessing/process.py
@@ -3,11 +3,9 @@
 
 def print_lines(file_path, x, y):
     with open(file_path, 'r') as file:
-        # ��ӡ�ļ���ǰ x ��
         for _ in range(x):
             print(file.readline().rstrip())
 
-        # ��ȡ�ļ�������������λ��ĩβ y ��֮ǰ��λ��
         file.seek(0, 2)
         end_pos = file.tell()
         file.seek(0, 0)
@@ -18,14 +16,14 @@ def print_lines(file_path, x, y):
             for _ in range(total_lines - y):
                 start_pos = file.readline()
 
-        # ��ӡ�ļ��ĺ� y ��
+
         file.seek(start_pos)
         for line in file:
             print(line.rstrip())
 
 
 def delete_lines(file_path, x):
-    # ɾ���ļ���ǰ x ��
+
     with open(file_path, 'r+') as file:
         lines = file.readlines()
         del lines[:x]
@@ -37,35 +35,33 @@ from collections import deque
 
 def print_first_and_last_lines(filename, x, y):
     with open(filename, 'r') as file:
-        # ��ȡǰ x ��
+
         print(f"First {x} lines:")
         for i in range(x):
             line = file.readline()
             if not line:
-                break  # �ļ���������x��
+                break  
             print(line.strip())
 
         print(f'\nLast {y} lines:')
-        # ��ȡ�ļ������ y ��
-        # deque ֻ������� y ������
+
         last_lines = deque(file, maxlen=y)
         for line in last_lines:
             print(line.strip())
 
 def delete_first_x_lines(filename, x):
     with open(filename, 'r') as file:
-        # ����ǰ x ��
+
         for _ in range(x):
             next(file, None)
         
-        # ��ʣ����б���
+
         remaining_lines = file.readlines()
     
-    # д��ʣ����У�����ԭ�ļ�
+
     with open(filename, 'w') as file:
         file.writelines(remaining_lines)
 
-# ���ú�����ָ���ļ�·����Ҫ��ӡ��ǰ x �кͺ� y ��
 file_path = 'graph.txt'
 print_first_and_last_lines(file_path, 5, 3)
 #delete_first_x_lines(file_path, 4)
